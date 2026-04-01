e215c4473c61fb5e09c76881a526cd6cf187495a
Robin Pacifico <123842499+Indifico@users.noreply.github.com>
2026-04-01 01:09:59 -0500

    read from file now stores total bits

--- src/functions.h
+++ src/functions.h
@@ -36,21 +36,26 @@ bool read_input_from_file(string filename, vector<symbol> symbol_vector)
         {
             symbol line_symbol; // temp var for the latest line's symbol
             line_symbol.frequency = 0;
+            line_symbol.n_bits_elias_gamma = 0;
 
             istringstream line_stream(file_line);
             line_stream >> line_symbol.data; // assigns leading character to the symbol's data char
             line_stream.ignore();
 
             cout << "Character: " << line_symbol.data << " ";
-            cout << "Encodings: ";
+            //cout << "Encodings: ";
             string encoding;
             while(getline(line_stream, encoding, ' '))
             {
                 line_symbol.frequency++;
                 line_symbol.elias_gamma_positions.push_back(encoding); // adds each encoding listed to position vector
-                cout << encoding << " ";
+                
+                line_symbol.n_bits_elias_gamma += encoding.length();
+                
+                //cout << encoding << " ";
             }
-            cout << "(Frequency: " << line_symbol.frequency << ")" << endl;
+            cout << "Frequency: " << line_symbol.frequency << " ";
+            cout << "Total Bits: " << line_symbol.n_bits_elias_gamma << endl;
         }
         return true;
     }
@@ -62,7 +67,10 @@ unsigned int elais_gamma_to_int(string encoding)
     istringstream encoding_stream(encoding);
     string zeros;
     getline(encoding_stream, zeros, '1');
+
     cout << zeros.length() << endl;
+    
+
     return 0;
 }
 
