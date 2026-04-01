1418600ae90763659c5e7f40a86bf93eebe2c3e0
Robin Pacifico <123842499+Indifico@users.noreply.github.com>
2026-04-01 03:33:41 -0500

    updated main; for loop in print results breaks

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,29 @@ int main()
     vector<symbol> alphabet;
     string filename;
     cin >> filename;
-    /* if (Call the read_input_from_file user-defined function here == true false)
+    if (read_input_from_file(filename, alphabet) == true)
     {
         string original_message;
-        //original_message.resize(call the original_message_size user-defined function here);
+        original_message.resize(original_message_size(alphabet));
         for (int i=0; i< alphabet.size();i++)
         {
-            // Call the decode_positions_and_populate_original_message user-defined function here
+            decode_positions_and_populate_original_message(alphabet.at(i), original_message);
         }
-        // Call the print_results user-defined function here
+        print_results(alphabet, original_message);
     }
     else
     {
         cout << "File not found." << endl;
     } 
-    */
 
     // testing purposes only
+    /*
     read_input_from_file(filename, alphabet);
     elais_gamma_to_int("0001010");
     elais_gamma_to_int("0001100");
     elais_gamma_to_int("00101");
-    elais_gamma_to_int("1"); 
+    elais_gamma_to_int("1");
+    */
+
     return 0;
 }
